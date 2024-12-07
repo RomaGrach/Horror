593360cc6eabd5ba2604e57077d29e0e712dc934
Salilonin <parshinakcyusha4@gmail.com>
2024-12-07 22:34:38 +0300

    не работает

--- Assets/myscripts/MonsterEvent.cs
+++ Assets/myscripts/MonsterEvent.cs
@@ -7,7 +7,7 @@ public class MonsterSpawner : MonoBehaviour
 {
     public GameObject monsterPrefab; // Префаб монстра
     public Camera playerCamera;      // Ссылка на камеру игрока
-    public float spawnDistance = 20f; // Дистанция от камеры, где появляется монстр
+    public float spawnDistance = 7f; // Дистанция от камеры, где появляется монстр
     public AudioClip scareSound;     // Страшный звук
     public float monsterLifetime = 5f; // Время, через которое монстр исчезнет
     public float lookSpeed = 2f;     // Скорость, с которой камера поворачивается на монстра
@@ -17,7 +17,7 @@ public class MonsterSpawner : MonoBehaviour
     private void OnTriggerEnter(Collider other)
     {
         // Если событие уже сработало или объект не камера, ничего не делаем
-        if (triggered || other.gameObject != playerCamera.gameObject)
+        if (triggered || other.gameObject.tag != "Player")
             return;
 
         triggered = true; // Устанавливаем, что триггер активирован
@@ -46,7 +46,7 @@ public class MonsterSpawner : MonoBehaviour
         StartCoroutine(LookAtMonster(monster.transform));
 
         // Уничтожаем монстра через заданное время
-        Destroy(monster, monsterLifetime);
+        /*Destroy(monster, monsterLifetime);*/
     }
 
     private IEnumerator LookAtMonster(Transform monster)
