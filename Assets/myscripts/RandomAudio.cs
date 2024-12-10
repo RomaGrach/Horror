67545af48177793694ceca281051c7e9c4a15fac
Salilonin <parshinakcyusha4@gmail.com>
2024-12-10 23:31:06 +0300

    6

--- Assets/myscripts/RandomAudio.cs
+++ Assets/myscripts/RandomAudio.cs
@@ -3,11 +3,14 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class RandomAudio : MonoBehaviour
+
 {
     public AudioClip[] audioClips; // Массив аудиоклипов
     public AudioSource audioSource; // Аудиоисточник
     public float minPause = 1f; // Минимальная пауза между звуками
     public float maxPause = 3f; // Максимальная пауза между звуками
+    public float minDistance = 1f; // Минимальное расстояние для громкости 100%
+    public float maxDistance = 5f; // Максимальное расстояние для полного затухания звука
 
     private void Start()
     {
@@ -18,12 +21,21 @@ public class RandomAudio : MonoBehaviour
 
         if (audioClips.Length > 0 && audioSource != null)
         {
+            ConfigureAudioSource();
             StartCoroutine(PlaySoundsRandomly());
         }
         else
         {
             Debug.LogError("Не заданы AudioClips или отсутствует AudioSource!");
         }
+    }
+
+    private void ConfigureAudioSource()
+    {
+        audioSource.spatialBlend = 1.0f; // Включаем 3D-звук
+        audioSource.rolloffMode = AudioRolloffMode.Linear; // Линейное затухание громкости
+        audioSource.minDistance = minDistance; // Радиус полной громкости
+        audioSource.maxDistance = maxDistance; // Радиус полного затухания
     }
 
     private IEnumerator PlaySoundsRandomly()
